c12de201d226c13383145ad5d6a8c102333b1cae
occmium <occkart@yahoo.com>
2019-10-19 23:52:16 +0600

    Task_63-4 Up

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -191,6 +191,27 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to(game_path(game))
     end
 
+    # Задача 63-4 — khsm: тест на GamesController#help
+    # Напишите тест на GamesController, который проверяет, что пользователь
+    # может воспользоваться подсказкой 50/50.
+    it 'uses fifty_fifty help' do
+      # сперва проверяем что в подсказках текущего вопроса пусто
+      expect(game_w_questions.current_game_question.help_hash[:fifty_fifty]).not_to be
+      expect(game_w_questions.fifty_fifty_used).to be_falsey
+
+      # фигачим запрос в контроллен с нужным типом
+      put :help, id: game_w_questions.id, help_type: :fifty_fifty
+      game = assigns(:game)
+
+      # проверяем, что игра не закончилась, что флажок установился, и подсказка записалась
+      expect(game.finished?).to be_falsey
+      expect(game.fifty_fifty_used).to be_truthy
+      expect(game.current_game_question.help_hash[:fifty_fifty]).to be
+      expect(game.current_game_question.help_hash[:fifty_fifty].size).to eq(2)
+      expect(game.current_game_question.help_hash[:fifty_fifty]).to include('d')
+      expect(response).to redirect_to(game_path(game))
+    end
+
     # Задача 62-3 — khsm: тест на GamesController#create (вторая игра)
     # Пользователь не может начать две игры. Если он начинает вторую,
     # его перенаправляют на первую
