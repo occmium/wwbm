17399d157e0af0a08f2a476d1eb7dcb6db7d9766
occmium <occkart@yahoo.com>
2019-10-20 10:43:40 +0600

    Task_64-1 Up

--- spec/views/user/show.html.erb_spec.rb
+++ spec/views/user/show.html.erb_spec.rb
@@ -4,31 +4,43 @@
 require 'rails_helper'
 
 RSpec.describe 'users/show', type: :view do
-  before(:each) do
-    assign(:user, FactoryBot.build_stubbed(:user, id: 1, name: 'Алёш'))
-    assign(:games, [FactoryBot.build_stubbed(:game)])
-    render
+  context 'any' do
+    before(:each) do
+      assign(:user, FactoryBot.build_stubbed(:user, name: 'Алёш'))
+
+      render
+    end
+
+    # любой пользователь видит свое имя
+    it 'sees his name' do
+      expect(rendered).to match 'Алёш'
+    end
+
+    # любой, не текущий пользователь не видит кнопку для смены пароля
+    it 'do not his change password' do
+      expect(rendered).not_to match /пароль/
+    end
   end
 
-  # пользователь видит свое имя
-  it 'sees his name' do
-    expect(rendered).to match 'Алёш'
-  end
+  context 'current user' do
+    before(:each) do
+      sign_in FactoryBot.create(:user)
+      assign(:user, FactoryBot.build_stubbed(:user, id: 1, name: 'Алёш'))
+      assign(:games, [FactoryBot.build_stubbed(:game)])
+      stub_template 'users/_game.html.erb' => 'User game goes here'
 
-  # текущий пользователь (и только он) видит кнопку для смены пароля
-  it 'only sees his change password' do
-    expect(rendered).not_to match /пароль/
+      render
+    end
 
-    sign_in FactoryBot.create(:user)
-    render
-    expect(rendered).to match /пароль/
-  end
+    # на странице отрисовываются фрагменты с игрой
+    it 'rendered partial' do
+      # А потом просто в тесте проверить
+      expect(rendered).to have_content 'User game goes here'
+    end
 
-  # на странице отрисовываются фрагменты с игрой
-  it 'rendered partial' do
-    stub_template 'users/_game.html.erb' => 'User game goes here'
-    render
-    # А потом просто в тесте проверить
-    expect(rendered).to have_content 'User game goes here'
+    # текущий пользователь видит кнопку для смены пароля
+    it 'sees change password' do
+      expect(rendered).to match /пароль/
+    end
   end
 end
